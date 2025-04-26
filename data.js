726bf5827aee8f54b2bb2666c8707b76cd093d48
Josiah Witt <josiah@witt.life>
2025-04-26 14:46:14 -0400

    fill in template

--- data.js
+++ data.js
@@ -1,41 +1,131 @@
 // @ts-check
-import { LandscapeSchema } from '@localfirstfm/landscape-schema'
+import { LandscapeSchema } from '@localfirstfm/landscape-schema';
 
 export const data = LandscapeSchema.make({
-	Version: 1,
-	Id: 'supersync',
-	Name: 'SuperSync',
-	Description: 'A cool way to sync data',
-	Website: 'https://supersync.cool',
-	Deployment: ['Self-hosted'],
-	License: 'MIT',
-	AppTarget: {
-		LanguageSDK: { data: ['typescript'] },
-	},
-	Networking: {
-		Topology: { data: 'Client-Server' },
-	},
-	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
-	},
-	ClientSideData: {
-		QueryAPI: { data: ['Async'] },
-		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
-		PersistenceFeatures: { data: 'Indexes' },
-		DataModel: { data: 'Document' },
-		OfflineReads: { data: 'Full Support' },
-		OfflineWrites: { data: 'Local conflict resolution' },
-		DataSize: { data: 'Up to 5-10 MB per document' },
-	},
-	SynchronizationStrategy: {
-		FullOrPartialReplication: { data: ['Full Replication'] },
-		ConflictHandling: { data: 'Automatic via CRDT' },
-		WhereResolutionOccurs: { data: 'Client' },
-		WhatGetsSynced: {
-			data: {
-				ClientToClient: 'Ops',
-			},
-		},
-	},
-})
+  Version: 1,
+  Id: 'dxos',
+  Name: 'DXOS',
+  Description: 'DXOS is an open source framework for building real-time, collaborative web applications.',
+  Website: 'https://dxos.org',
+  License: 'MIT',
+  Deployment: ['Self-hosted'],
+  AppTarget: {
+    Platform: {
+      data: ['Browser', 'Node'],
+    },
+    LanguageSDK: {
+      data: ['TypeScript'],
+    },
+  },
+  Networking: {
+    Protocol: {
+      data: ['WebRTC', 'WebSockets'],
+      comment: 'P2P connections are made via WebRTC, and client-server connections are made via WebSockets.',
+    },
+    Topology: {
+      data: 'P2P',
+      comment: 'Always available data optionally provided by server-based peers.',
+    },
+  },
+  ServerSideData: {
+    PersistenceMechanism: {
+      data: ['N/A'],
+    },
+    DataModelParadigm: {
+      data: 'Document',
+    },
+  },
+  ClientSideData: {
+    QueryAPI: {
+      data: ['Async', 'Signals-based Reactivity'],
+    },
+    LocalRefreshLatency: {
+      data: '10-50ms',
+      comment: 'Data is stored as Automerge documents under the hood.',
+    },
+    PersistenceMechanism: {
+      data: ['IndexedDB'],
+    },
+    PersistenceFeatures: {
+      data: 'Indexes',
+      comment: 'Custom indexes coming soon.',
+    },
+    DataModel: {
+      data: 'Document',
+    },
+    SchemaManagement: {
+      data: ['Schema definition', 'Schema migrations', 'Schema validation on write'],
+      comment:
+        'Schemas are defined using Effect Schema, and are validated on write. Migrations are supported at the object level.',
+    },
+    OfflineReads: {
+      data: 'Full Support',
+    },
+    OptimisticUpdates: {
+      data: 'Yes',
+    },
+    OfflineWrites: {
+      data: 'Full local conflict resolution',
+    },
+    DataSize: {
+      data: 'up to 5-10mb per object',
+      comment: 'Objects are Automerge documents under the hood.',
+    },
+  },
+  SynchronizationStrategy: {
+    FullOrPartialReplication: {
+      data: ['Full Replication'],
+      comment: 'Full replication of all spaces joined per identity.',
+    },
+    ConflictHandling: {
+      data: 'Automatic via CRDT',
+      comment: 'Automerge is the CRDT used under the hood.',
+    },
+    WhereResolutionOccurs: {
+      data: 'Client',
+    },
+    WhatGetsSynced: {
+      data: {
+        ClientToClient: 'see Automerge',
+      },
+      comment: 'Automerge is the CRDT used under the hood.',
+    },
+    Authority: {
+      data: 'Decentralized',
+    },
+  },
+  AuthIdentity: {
+    Encryption: {
+      data: 'Transport-level encryption',
+      comment: 'Data is not currently encrypted at rest. E2EE is planned via Keyhive.',
+    },
+    AuthenticationMethod: {
+      data: ['Public keys'],
+    },
+    AuthorizationPermissions: {
+      data: 'RBAC',
+    },
+  },
+  UIRelated: {
+    RichTextEditing: {
+      data: 'Yes',
+      comment: 'Anything compatible with Automerge documents (e.g. CodeMirror, ProseMirror, etc.)',
+    },
+    Components: {
+      data: ['Invitations', 'Presence'],
+      comment:
+        'React components for performing invitations are provided. SDK provides which peers you are connected to.',
+    },
+  },
+  DevelopmentWorkflowsDX: {
+    DebuggingTools: {
+      data: ['DevTools', 'Data Inspector', 'Network Inspector'],
+    },
+    CLI: {
+      data: 'CLI which contains a full client and commands for managing identities, spaces, etc.',
+    },
+    TypeSupport: {
+      data: 'Full type support via Effect Schema',
+    },
+  },
+});
--- details.md
+++ details.md
@@ -1,15 +1,15 @@
-# SuperSync
+# DXOS
 
-SuperSync is a cool way to sync your data.
+The new standard for building collaborative local-first software.
+
+DXOS is an open source framework for building real-time, collaborative web applications that run entirely on the client and communicate peer-to-peer, without the need for centralized servers.
+
+To show off the power of DXOS, we've built [Composer](https://dxos.org/composer). A local-first workspace that is designed for small team collaboration.
 
 ## Features
 
-- **SuperSync**: Sync your data with ease.
-- **Local-first**: Your data is stored locally, ensuring privacy and security.
-- **Cross-platform**: Works on all major platforms.
-- **Open-source**: The code is available for anyone to use and contribute to.
-- **Community-driven**: Join our community and help us improve the project.
+- **Client-side databases that scale**: ECHO is an open source database architecture that incorporates transparent data replication and conflict resolution for secure and scalable local storage.
+- **Seamless Public/Private key identities**: HALO seamlessly manages digital identities using public/private key pairs allowing nuanced control over access to applications, databases, and networked devices.
+- **Performant Peer-to-Peer networks**: MESH extends existing internet protocols to enable secure and resilient peer-to-peer networks without the need for centralized server infrastructure.
+- **Seamless Collaboration**: Add realtime collaboration to your local-first application without the complexity of building your own sync engine.
 
-## More info
-
-...
